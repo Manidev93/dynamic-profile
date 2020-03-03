634410f01c64a92baaf55be0ae510dd686dfe817
Manikandan Devendran <md649@uowmail.edu.au>
2020-03-03 20:37:27 +1100

    Merge changes

---
+++ pages/user/ben-zhang-10.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{$nuxt.$route.name}}</h1>
+</template>
+
+<script>
+export default {
+    head() {
+        return{
+            title: $nuxt.$route.name,
+            meta: [
+                { hid: 'description', name: 'description', content: 'description' }
+            ]
+        }
+    }
+}
+</script>
